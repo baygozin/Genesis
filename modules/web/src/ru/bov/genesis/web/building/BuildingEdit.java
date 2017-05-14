e593535192ce39f85af578feeb81ab048fdfc0d1
Oleg Baygozin <oleg.baygozin@yandex.ru>
2017-05-14 14:57:11 +0500

    Сделал вроде как график вахт (в первом приближении)

--- modules/web/src/ru/bov/genesis/web/building/BuildingEdit.java
+++ modules/web/src/ru/bov/genesis/web/building/BuildingEdit.java
@@ -1,6 +1,7 @@
 package ru.bov.genesis.web.building;
 
 import com.haulmont.bali.util.ParamsMap;
+import com.haulmont.charts.gui.amcharts.model.Color;
 import com.haulmont.charts.gui.components.charts.Chart;
 import com.haulmont.charts.gui.components.map.MapViewer;
 import com.haulmont.charts.gui.data.ListDataProvider;
@@ -15,10 +16,14 @@ import ru.bov.genesis.entity.mainentity.Building;
 import ru.bov.genesis.entity.mainentity.Employee;
 
 import javax.inject.Inject;
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.joda.time.*;
+
+import static ru.bov.genesis.Func.fullFIO;
+import static ru.bov.genesis.Func.shortFIO;
 
 public class BuildingEdit extends AbstractEditor<Building> {
 
@@ -52,55 +57,69 @@ public class BuildingEdit extends AbstractEditor<Building> {
     @Inject
     private Chart ganttChart;
 
+    private int lengthCalendar; // Ширина отображаемой диаграммы в месяцах
+    private int minusCalc = 0; // Начало расчета сегментов в месяцах от сегодня
+    private int plusCalc = 4; // Конец расчета сегментов в месяцах от сегодня
+    private String colorWork = "#008800";
+    private String colorPause = "#880000";
+
+
+    // Расчитаем сегменты для диаграммы
+    private List<MapDataItem> calcSegments(LocalDate dateStart,
+                                           LocalDate dateEnd,
+                                           int periodWork,
+                                           int periodPause) {
+        LocalDate now = new LocalDate();
+        LocalDate startCalc = now.minusMonths(minusCalc); // начало расчета сегментов - текущая дата минус "minusCalc"
+        LocalDate stopCalc = now.plusMonths(plusCalc);    // конец расчета сегментов - текущая дата плюс "plusCalc"
+        List<MapDataItem> segments = new ArrayList<>();
+
+        LocalDate start = dateStart;
+        LocalDate stop = dateEnd;
+        for (LocalDate dateI = startCalc; dateI.isBefore(stopCalc); ) {
+            stop = start.plusMonths(periodWork);
+            segments.add(setItemWork(start, stop));
+            start = stop;
+            stop = start.plusMonths(periodPause);
+            segments.add(setItemPause(start.plusDays(1), stop.minusDays(1)));
+            start = stop;
+            dateI = dateI.plusMonths(3);
+        }
+        return segments;
+    }
+
+    private MapDataItem setItemWork(LocalDate start, LocalDate end) {
+        return new MapDataItem(ParamsMap.of("start", start.toString(),
+                "end", end.toString(), "task",
+                "Вахта", "color", colorWork));
+    }
+
+    private MapDataItem setItemPause(LocalDate start, LocalDate end) {
+        return new MapDataItem(ParamsMap.of("start", start.toString(),
+                "end", end.toString(), "task",
+                "Межвахта", "color", colorPause));
+    }
+
     @Override
     public void init(Map<String, Object> params) {
 
         ListDataProvider dataProvider = new ListDataProvider();
         List<MapDataItem> segments = new ArrayList<>();
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-01",
-                "end", "2016-01-14", "task", "Уборка територии", "color", "#b9783f")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-16",
-                "end", "2016-01-27", "task", "Отхожий промысел")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-05",
-                "end", "2016-04-18", "task", "Заготовка дров")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-04-18",
-                "end", "2016-04-30", "task", "Бездельничаем")));
-        dataProvider.addItem(new MapDataItem(ParamsMap.of("category", "Дядя Петя", "segments", segments)));
-
-        segments = new ArrayList<>();
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-01",
-                "end", "2016-01-10", "task", "Варим борЖч", "color", "#cc4748")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-12",
-                "end", "2016-01-15", "task", "Лепим пельмени")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-16",
-                "end", "2016-02-05", "task", "Пришиваем заплаты")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-10",
-                "end", "2016-02-18", "task", "Извращаемся")));
-        dataProvider.addItem(new MapDataItem(ParamsMap.of("category", "Тетя Мотя", "segments", segments)));
-
-        segments = new ArrayList<>();
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-01", "end", "2016-02-10",
-                "task", "Вахта", "color", "#00aa00")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-10", "end", "2016-02-28",
-                "task", "Межахта", "color", "#990000")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-28", "end", "2016-03-30",
-                "task", "Вахта", "color", "#00aa00")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-03-30", "end", "2016-04-30",
-                "task", "Межвахта", "color", "##990000")));
-        dataProvider.addItem(new MapDataItem(ParamsMap.of("category", "сосед Филаретыч", "segments", segments)));
-
-        segments = new ArrayList<>();
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-01-01", "end", "2016-02-10",
-                "task", "Вахта", "color", "#990000")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-10", "end", "2016-02-28",
-                "task", "Межахта", "color", "#00aa00")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-02-28", "end", "2016-03-30",
-                "task", "Вахта", "color", "#990000")));
-        segments.add(new MapDataItem(ParamsMap.of("start", "2016-03-30", "end", "2016-04-30",
-                "task", "Межвахта", "color", "##00aa00")));
-        dataProvider.addItem(new MapDataItem(ParamsMap.of("category", "папа Римский", "segments", segments)));
+        ganttChart.setVisible(false);
+        if (!((Building) params.get("ITEM")).getEmployeeCk().isEmpty()) {
+            ganttChart.setVisible(true);
+            for (Employee employee : ((Building) params.get("ITEM")).getEmployeeCk()) {
+                dataProvider.addItem(new MapDataItem(ParamsMap.of("category",
+                        shortFIO(employee.getLastName(), employee.getFirstName(), employee.getMiddleName()),
+                        "segments", calcSegments(
+                                new LocalDate(employee.getDateWorkStart()),
+                                new LocalDate(employee.getDateWorkEnd()),
+                                2, 1))));
+            }
+        }
 
         ganttChart.setDataProvider(dataProvider);
+
 
         TextField place = (TextField) fieldGroup.getField("place").getComponent();
         place.addTextChangeListener(new TextInputField.TextChangeListener() {
@@ -115,8 +134,8 @@ public class BuildingEdit extends AbstractEditor<Building> {
             public void onClick(MapClickEvent event) {
                 Double latitude = event.getPosition().getLatitude();
                 Double longtitude = event.getPosition().getLongitude();
-                String pl1 = String.format("%.6f", latitude).replace(",",".");
-                String pl2 = String.format("%.6f", longtitude).replace(",",".");
+                String pl1 = String.format("%.6f", latitude).replace(",", ".");
+                String pl2 = String.format("%.6f", longtitude).replace(",", ".");
                 place.setValue(pl1 + ", " + pl2);
             }
         });
@@ -140,7 +159,7 @@ public class BuildingEdit extends AbstractEditor<Building> {
         dataGridEmployeeCK.addGeneratedColumn("fieldFio", new DataGrid.ColumnGenerator<Employee, String>() {
             @Override
             public String getValue(DataGrid.ColumnGeneratorEvent<Employee> event) {
-               return Func.fullFIO(event.getItem().getLastName(), event.getItem().getFirstName(), event.getItem().getMiddleName());
+                return fullFIO(event.getItem().getLastName(), event.getItem().getFirstName(), event.getItem().getMiddleName());
             }
 
             @Override
