d4c29f22a36a5ef7c4ad00d7c78a74fe72f16118
Oleg Baygozin <oleg.baygozin@yandex.ru>
2017-05-26 16:43:11 +0500

    Очередное изменение

--- modules/web/src/ru/bov/genesis/web/employee/EmployeeBrowse.java
+++ modules/web/src/ru/bov/genesis/web/employee/EmployeeBrowse.java
@@ -2,11 +2,12 @@ package ru.bov.genesis.web.employee;
 
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
-import ru.bov.genesis.Func;
 import ru.bov.genesis.entity.mainentity.Employee;
 
 import javax.inject.Inject;
 import java.util.Map;
+
+import static ru.bov.genesis.utils.GlobalTools.returnStatusColor;
 
 public class EmployeeBrowse extends AbstractLookup {
 
@@ -21,20 +22,12 @@ public class EmployeeBrowse extends AbstractLookup {
 
     @Override
     public void init(Map<String, Object> params) {
-        employeesTable.setStyleProvider((entity, property) -> {
-                if (entity.getBuilding() != null) {
-                    return Func.returnStatusColor(true, property, entity.getDateWorkStart(), entity.getDateWorkEnd());
-                } else {
-                    return Func.returnStatusColor(false, property, entity.getDateWorkStart(), entity.getDateWorkEnd());
-                }
-        });
-        
+        employeesTable.setSortable(true);
+        employeesTable.setColumnSortable("fullName", true);
+
+        employeesTable.setStyleProvider(((entity, property) -> {
+                return returnStatusColor(property, entity);
+        }));
     }
 
-    public Component generateFullNameCell(Employee entity) {
-        Label field = (Label) componentsFactory.createComponent(Label.NAME);
-        employeesTable.getColumn("fullName").setCaption("ФИО");
-        field.setValue(Func.fullFIO(entity.getLastName(), entity.getFirstName(), entity.getMiddleName()));
-        return field;
-    }
 }
