69a8f0c6ae345ed9ae276f53291b450ca2d77422
Oleg Baygozin <oleg.baygozin@yandex.ru>
2017-06-06 21:55:32 +0500

    Очередное изменение

--- modules/web/src/ru/bov/genesis/web/employee/EmployeeEdit.java
+++ modules/web/src/ru/bov/genesis/web/employee/EmployeeEdit.java
@@ -8,20 +8,30 @@ import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.export.ExportDisplay;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
-import ru.bov.genesis.Func;
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+import org.joda.time.Years;
 import ru.bov.genesis.entity.mainentity.Employee;
 import ru.bov.genesis.entity.services.StorageFiles;
 
+import javax.ejb.Local;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.ByteArrayInputStream;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.Map;
 
+import static ru.bov.genesis.utils.GlobalTools.fullFIO;
+
 public class EmployeeEdit extends AbstractEditor<Employee> {
+
+    @Inject
+    private DateField udCkDateExpire;
     @Inject
     private Embedded imagePhoto;
+
+    @Inject
+    private Embedded imageSign;
 
     @Inject
     private Datasource<Employee> employeeDs;
@@ -42,7 +52,19 @@ public class EmployeeEdit extends AbstractEditor<Employee> {
     private Label labelFio;
 
     @Inject
+    private Label labelStatus;
+
+    @Inject
+    private Label labelNow;
+
+    @Inject
+    private Label ageMan;
+
+    @Inject
     private FieldGroup fieldGroupPersonal;
+
+    @Inject
+    private FieldGroup pasportGroup;
 
     @Inject
     private ExportDisplay exportDisplay;
@@ -54,15 +76,37 @@ public class EmployeeEdit extends AbstractEditor<Employee> {
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
-        FileDescriptor imageFile = ((Employee) params.get("ITEM")).getImage();
-        loadImage(imageFile);
+
+
+
+        if (((Employee) params.get("ITEM")).getBuilding() == null) {
+            labelStatus.setValue(", не закреплен за объектом");
+        } else {
+            labelStatus.setValue(", закреплен за: ");
+        }
+
+        Date birth = ((Employee) params.get("ITEM")).getBirthday();
+        FieldGroup.FieldConfig config = pasportGroup.getField("ageMan");
+        Label labelAge = (Label) config.getComponent();
+        labelAge.setStyleName("h2");
+        if (birth != null) {
+            labelAge.setValue(getAgeMan(birth));
+        }
+
+        labelNow.setValue(LocalDate.now().toString("сегодня d MMMM YYYY года"));
+
+        FileDescriptor imageFilePhoto = ((Employee) params.get("ITEM")).getImage_photo();
+        FileDescriptor imageFileSign = ((Employee) params.get("ITEM")).getImage_sign();
+        loadImage(imagePhoto, imageFilePhoto);
+        loadImage(imageSign, imageFileSign);
 
         employeeDs.addItemPropertyChangeListener(event -> {
-            loadImage(event.getItem().getImage());
+            loadImage(imagePhoto, event.getItem().getImage_photo());
+            loadImage(imageSign, event.getItem().getImage_sign());
         });
 
         String fio = "<h1><div align=\"center\">" +
-                Func.fullFIO(((Employee) params.get("ITEM")).getLastName(),
+                fullFIO(((Employee) params.get("ITEM")).getLastName(),
                 ((Employee) params.get("ITEM")).getFirstName(),
                 ((Employee) params.get("ITEM")).getMiddleName())
                 + "</div></h1>";
@@ -70,12 +114,12 @@ public class EmployeeEdit extends AbstractEditor<Employee> {
 
     }
 
-    private void loadImage(FileDescriptor imageFile) {
+    private void loadImage(Embedded image, FileDescriptor imageFile) {
         if (imageFile == null) {
-            imagePhoto.setVisible(false);
+            image.setVisible(false);
             return;
         } else {
-            imagePhoto.setVisible(true);
+            image.setVisible(true);
         }
         byte[] bytes = null;
         try {
@@ -84,16 +128,27 @@ public class EmployeeEdit extends AbstractEditor<Employee> {
             showNotification("Файл не загружен", NotificationType.HUMANIZED);
         }
         if (bytes != null) {
-            imagePhoto.setSource(imageFile.getName(), new ByteArrayInputStream(bytes));
+            image.setSource(imageFile.getName(), new ByteArrayInputStream(bytes));
         } else {
-            imagePhoto.setVisible(false);
+            image.setVisible(false);
         }
     }
 
     public void openFile(Entity item, String columnId) {
-        //FileDescriptor file = ((Employee) item).getStorageFile()
         exportDisplay.show(((StorageFiles) item).getFile());
-        //showNotification("Clicked by " + columnId, NotificationType.HUMANIZED);
+    }
+
+
+    public Component ageManFunction(Datasource datasource, String fieldId) {
+        Label age = componentsFactory.createComponent(Label.class);
+        //age.setDatasource(datasource, fieldId);
+		return age;
+    }
+
+    public String getAgeMan(Date birthday) {
+        LocalDate ageMan = LocalDate.fromDateFields(birthday);
+        Years years = Years.yearsBetween(ageMan, LocalDate.now());
+        return String.valueOf(years.getYears());
     }
 
 }
